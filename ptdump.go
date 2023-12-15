78a6d059371776c3297b641deee6dc7e66a30d6e
vilr0i <vilr0i@protonmail.com>
2023-12-14 22:38:47 -0800

    Implemented getMaps() which parses /proc/[pid]/maps of a given process

---
+++ ptdump.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type MapEntry struct {
+	StartAddr uint
+	EndAddr   uint
+	Perms     string
+	Offset    uint
+	DevMajor  int
+	DevMinor  int
+	Inode     uint
+	Path      string
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s pid\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	pid, err := strconv.Atoi(os.Args[1])
+	check(err)
+
+	maps := getMaps(pid)
+	fmt.Println(maps)
+}
+
+func getMaps(pid int) []MapEntry {
+	path := fmt.Sprintf("/proc/%d/maps", pid)
+	f, err := os.Open(path)
+	check(err)
+
+	scanner := bufio.NewScanner(f)
+
+	var maps []MapEntry
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		var ent MapEntry
+		_, err := fmt.Sscanf(line, "%x-%x %s %x %x:%x %d %s", &ent.StartAddr, &ent.EndAddr, &ent.Perms,
+			&ent.Offset, &ent.DevMajor, &ent.DevMinor, &ent.Inode, &ent.Path)
+
+		if err == io.EOF {
+			_, err = fmt.Sscanf(line, "%x-%x %s %x %x:%x %d", &ent.StartAddr, &ent.EndAddr, &ent.Perms,
+				&ent.Offset, &ent.DevMajor, &ent.DevMinor, &ent.Inode)
+		}
+		check(err)
+
+		maps = append(maps, ent)
+	}
+
+	return maps
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
