7f479fdde510c831fbd07dedee44cb2fe4161828
vilr0i <vilr0i@protonmail.com>
2023-12-14 23:58:23 -0800

    implemented getPageInfo(), which gets the entry in /proc/[pid]/pagemap for the corresponding virtual address of a process

--- ptdump.go
+++ ptdump.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"os"
@@ -19,6 +20,17 @@ type MapEntry struct {
 	Path      string
 }
 
+type PageType int
+
+type PageInfo struct {
+	Present      bool
+	IsSwapped    bool
+	IsFileOrAnon bool
+	WriteProt    bool
+	MapExcl      bool
+	Addr         uint
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "usage: %s pid\n", os.Args[0])
@@ -29,7 +41,41 @@ func main() {
 	check(err)
 
 	maps := getMaps(pid)
-	fmt.Println(maps)
+	for _, m := range maps {
+		pageinfo := getPageInfo(pid, m.StartAddr)
+		fmt.Printf("%+v\n", m)
+		fmt.Printf("%+v\n\n", pageinfo)
+	}
+}
+
+// TODO: change from hardcoded value to something arch dependent
+// (8 bytes assumes 64 bit
+func getPageInfo(pid int, virt_addr uint) PageInfo {
+	path := fmt.Sprintf("/proc/%d/pagemap", pid)
+	f, err := os.Open(path)
+	check(err)
+
+	page := (virt_addr & ^(uint(0x1000 - 1))) / 0x1000
+	offset := int64(page * 8)
+	_, err = f.Seek(offset, os.SEEK_SET)
+	check(err)
+
+	buf := make([]byte, 8)
+	_, err = f.Read(buf)
+	check(err)
+
+	info := binary.LittleEndian.Uint64(buf)
+	//fmt.Printf("0b%b\n", info)
+
+	var pageinfo PageInfo
+	pageinfo.Present = btoi(int(info >> 63))
+	pageinfo.IsSwapped = btoi(int(info>>62) & 1)
+	pageinfo.IsFileOrAnon = btoi(int(info>>61) & 1)
+	pageinfo.WriteProt = btoi(int(info>>57) & 1)
+	pageinfo.MapExcl = btoi(int(info>>56) & 1)
+	pageinfo.Addr = uint(info & ((1 << 56) - 1))
+
+	return pageinfo
 }
 
 func getMaps(pid int) []MapEntry {
@@ -57,6 +103,13 @@ func getMaps(pid int) []MapEntry {
 	}
 
 	return maps
+}
+
+func btoi(i int) bool {
+	if i == 0 {
+		return false
+	}
+	return true
 }
 
 func check(err error) {
