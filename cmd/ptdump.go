10a823c099a7c94f9cbe2436737a2f7c49713cd7
vilr0i <vilr0i@protonmail.com>
2023-12-16 10:23:09 -0800

    Cleaned up output format.

--- cmd/ptdump.go
+++ cmd/ptdump.go
@@ -16,10 +16,10 @@ func main() {
 	check(err)
 
 	maps := getMaps(pid)
-	fmt.Println("virt Addr\t\tphysical addr\t\tsize\t\tperms\t\tpresent\t\tswapped")
+	fmt.Printf("%v%30v%13v%15v%15v%15v%11v\n", "virt addr", "physical addr", "size", "perms", "present", "swapped", "path")
 	for _, m := range maps {
 		pageinfo := getPageInfo(pid, m.StartAddr)
-		fmt.Printf("0x%x\t\t0x%x\t\t%d\t\t%s\t\t%t\t\t%t\t\t%s\n",
+		fmt.Printf("%0#x%#20x%20d%12s%13t%16t\t\t%s\n",
 			m.StartAddr, pageinfo.Addr, (m.EndAddr - m.StartAddr),
 			m.Perms, pageinfo.Present, pageinfo.IsSwapped, m.Path)
 	}
