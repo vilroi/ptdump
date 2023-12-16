0e9a33464a79988c64bd7afcc4c572c1db18574e
vilr0i <vilr0i@protonmail.com>
2023-12-16 15:46:48 -0800

    Updated message shown for the usage.

--- cmd/ptdump.go
+++ cmd/ptdump.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/vilroi/ptdump/internal/pagetable"
@@ -9,8 +11,13 @@ import (
 
 func main() {
 	init_flags()
+
 	flag.Parse()
 	args := flag.Args()
+	if len(args) != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	pid, err := strconv.Atoi(args[0])
 	check(err)
@@ -27,4 +34,8 @@ func check(err error) {
 
 func init_flags() {
 	flag.BoolVar(&pagetable.GranularFlag, "g", false, "Granular output: show all individual pages, instead of them being coaleced")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: %s [opts] pid\n", os.Args[0])
+		flag.PrintDefaults()
+	}
 }
